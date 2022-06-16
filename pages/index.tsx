55615cad779526d4ff64bc63a21630839ccdfcee
Top-Kraken <topkyle521@gmail.com>
2022-06-16 16:29:18 -0400

    fix wallet connection issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,21 +2,23 @@ import React from 'react'
 import type { NextPage } from 'next'
 import MetaMaskConnect from '../components/MetaMaskConnect'
 import Tabs from '../components/Tabs'
-import { WalletProvider } from '../components/WalletProvider'
-import useWallet  from '../hooks/useWallet'
+import useWallet from '../hooks/useWallet'
 
 const Home: NextPage = () => {
   const context = useWallet()
   const [isBlur, setIsBlur] = React.useState<boolean>(false)
-  React.useEffect(()=>{
-    setIsBlur(context.signer?false:true)
+  React.useEffect(() => {
+    setIsBlur(context.signer ? false : true)
   }, [context.signer])
   return (
     <>
-      <WalletProvider>
-        <MetaMaskConnect onConnect={async ()=>{context.connect()}} context={context}/>
-        <Tabs blur={isBlur}/>
-      </WalletProvider>
+      <MetaMaskConnect
+        onConnect={async () => {
+          context.connect()
+        }}
+        context={context}
+      />
+      <Tabs blur={isBlur} />
     </>
   )
 }
