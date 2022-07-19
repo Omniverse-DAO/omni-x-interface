17efd8089d762ba3c4839e868424213cc6b082f2
Top-Kraken <topkyle521@gmail.com>
2022-07-19 12:45:59 -0700

    update api endpoint

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   env: {
-    API_BASE_URL: 'https://omni-x-api.herokuapp.com/api/v1/',
-    API_URL: 'https://omni-x-api.herokuapp.com/'
+    API_BASE_URL: 'http://34.227.14.254/api/v1/',
+    API_URL: 'http://34.227.14.254/'
   },
   images: {
-    domains: ['localhost', 'omni-x-api.herokuapp.com'],
+    domains: ['localhost', '34.227.14.254'],
     loader: 'akamai',
     path: ''
   }
