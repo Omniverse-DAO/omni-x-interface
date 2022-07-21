1e7aebbab8fb4ec944a28d7be78aed28b3c54e13
Top-Kraken <topkyle521@gmail.com>
2022-07-21 16:40:58 -0400

    update api config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    API_BASE_URL: 'http://localhost:5000/api/v1/',
+    API_BASE_URL: 'https://api.omni-x.io/api/v1/',
     API_URL: 'https://omni-x.s3.amazonaws.com/'
   },
   images: {
